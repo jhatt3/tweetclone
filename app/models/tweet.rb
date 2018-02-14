c731a3abe2958421003eaeac6c5c74a85a4081ac
Jesse Hatton <odloads@live.com>
2018-02-14 08:31:18 -0500

    add username and message length validation

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,7 @@
 class Tweet < ApplicationRecord
     belongs_to :user
+
+    validates :message, presence: true
+    validates :message, length: {maximum: 80, too_long: "A Tweet is only 80 chars Max."}
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ApplicationRecord
   has_many :tweets
 
   mount_uploader :avatar, AvatarUploader
+
+  validates :username, presence: true, uniqueness: true
 end
