aecde1beefe13b705b5b2122dfcbbb80b4c92836
Jesse Hatton <odloads@live.com>
2018-02-15 09:16:37 -0500

    adding tags and joining relational tables in db. many to many relationships...

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -26,7 +26,23 @@ class TweetsController < ApplicationController
   # POST /tweets
   # POST /tweets.json
   def create
-    @tweet = Tweet.new(tweet_params)
+    @tweet = Tweet.create(tweet_params)
+
+    message_arr = @tweet.message.split
+
+    message_arr.each_with_index do |word, index|
+      if word[0] == '#'
+        if Tag.pluck(:phrase).include?(word)
+          tag = Tag.find_by(phrase: word)
+        else
+          tag = Tag.create(phrase: word)
+        end
+          tweet_tag = TweetTag.create(tweet_id: @tweet.id, tag_id: tag.id)
+          # query string added to message array.
+          message_arr[index] = "<a href='/tag_tweets?id=#{tag.id}'>#{word}</a>"
+      end
+    end
+    @tweet.update(message: message_arr.join(" "))
 
     respond_to do |format|
       if @tweet.save
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,9 @@
 class Tweet < ApplicationRecord
     belongs_to :user
 
+    has_many :tweet_tags
+    has_many :tweets, through: :tweet_tags
+
     validates :message, presence: true
     validates :message, length: {maximum: 80, too_long: "A Tweet is only 80 chars Max."}
 
